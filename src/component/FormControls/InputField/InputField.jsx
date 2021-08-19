2a786bffdb91fef3699809bad29384d2c29229e8
namthanhtran <namthanhtran16@gmail.com>
2021-08-19 19:39:22 +0700

    change InputField

--- src/component/FormControls/InputField/InputField.jsx
+++ src/component/FormControls/InputField/InputField.jsx
@@ -1,44 +1,45 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Controller } from 'react-hook-form';
 import { TextField } from '@material-ui/core';
+import PropTypes from 'prop-types';
+import React from 'react';
+import { Controller } from 'react-hook-form';
 
 InputField.propTypes = {
   form: PropTypes.object.isRequired,
   name: PropTypes.string.isRequired,
 
   label: PropTypes.string,
-  disabled: PropTypes.bool,
+  disable: PropTypes.bool,
 };
 
 function InputField(props) {
-  const { form, name, label, disabled } = props;
-  const control = form;
+  const { form, name, label, disable } = props;
+  const { control } = form;
   
   return (
     <>
-      <Controller 
-        name={name}
-        control={control}
-        render={({
-          field: {onChange, onBlur, value, name},
-          fieldState: {invalid, error},
-        }) => (
-          <TextField 
-            margin="normal"
-            variant="outlined"
-            fullWidth
-            onChange={onChange}
-            onBlur={onBlur}
-            label={label}
-            value={value}
-            name={name}
-            disabled={disabled}
-            error={invalid}
-            helperText={error?.message}
-          />
-        )}
-      /> 
+      <Controller
+      name={name}
+      control={control}
+      render={({
+        field: { onChange, onBlur, value, name }, 
+        fieldState: { invalid, error } 
+      }) => (
+        <TextField
+          margin="normal"
+          variant="outlined"
+          fullWidth
+
+          name={name}
+          value={value}
+          label={label}
+          error={invalid}
+          disabled={disable}
+          onChange={onChange}
+          onBlur={onBlur}
+          helperText={error?.message}
+        />
+      )}
+    />
     </>
   );
 }
