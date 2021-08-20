b9bc41057e6cd9e5c7e01f4e8c9420a6ac643130
namthanhtran <namthanhtran16@gmail.com>
2021-08-20 13:17:37 +0700

    dialog form login/register

---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,74 @@
+import { makeStyles } from '@material-ui/core';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import React from 'react';
+import Register from '../../features/Auth/Register/Register';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+Header.propTypes = {
+  
+};
+
+function Header(props) {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  return (
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          
+          <Typography variant="h6" className={classes.title}>
+            E-Commerce
+          </Typography>
+
+          <Button color="inherit" onClick={handleClickOpen}>Register</Button>
+        </Toolbar>
+      </AppBar>
+
+      
+
+      <Dialog open={open} 
+              onClose={handleClose} 
+              aria-labelledby="form-dialog-title"
+              disableEscapeKeyDown
+              disableBackdropClick >
+          <DialogTitle id="form-dialog-title">Register</DialogTitle>
+          <DialogContent>
+            <Register />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} color="primary">
+              Cancel
+            </Button>
+          </DialogActions>
+        </Dialog>
+    </div>
+  );
+}
+
+export default Header;
