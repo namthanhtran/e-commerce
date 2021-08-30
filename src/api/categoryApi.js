c7c71b4e986ed7baa46e698bbc817269e6957f80
namthanhtran <namthanhtran16@gmail.com>
2021-08-30 22:46:23 +0700

    setup category API

---
+++ src/api/categoryApi.js
@@ -0,0 +1,14 @@
+import axiosClient from "./axiosClient";
+
+const categoryApi = {
+  getAll(params){
+    const url = '/categories';
+    return axiosClient.get(url, params);
+  },
+  get(id){
+    const url = `/categories/${id}`;
+    return axiosClient.get(url);
+  }
+}
+
+export default categoryApi;
