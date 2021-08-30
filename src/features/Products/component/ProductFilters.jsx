87312133f26214233dbc6926ee74cd749e805f46
namthanhtran <namthanhtran16@gmail.com>
2021-08-30 22:47:02 +0700

    add filter by category, price, service

---
+++ src/features/Products/component/ProductFilters.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Box, Paper } from '@material-ui/core';
+import FilterByCategory from './Filters/FilterByCategory';
+import FilterByPrice from './Filters/FilterByPrice';
+import FilterByService from './Filters/FilterByService';
+
+ProductFilters.propTypes = {
+  filters: PropTypes.object.isRequired,
+  onChange: PropTypes.func,
+};
+
+function ProductFilters({filters, onChange}) {
+
+  const handleCategoryChange = (newCategoryid) => {
+    if(!onChange) return;
+
+    const newFilters = {
+      // ...filters,
+      'category.id': newCategoryid,
+    }
+    onChange(newFilters);
+  };
+
+  const handleChange = (values) => {
+    if(onChange) onChange(values);
+  }
+
+  return (
+    <Box>
+      <Paper elevation={0} variant='outlined' square>
+        <FilterByCategory onChange={handleCategoryChange} />
+      </Paper>
+      <Paper elevation={0} variant='outlined' square>
+        <FilterByPrice onChange={handleChange} />
+      </Paper>
+      <Paper elevation={0} variant='outlined' square>
+        <FilterByService filters={filters} onChange={handleChange} />
+      </Paper>
+    </Box>
+  );
+}
+
+export default ProductFilters;
