afa4d0d3e3e98cb850f3a0ee317338dbc428746f
namthanhtran <namthanhtran16@gmail.com>
2021-09-01 13:42:55 +0700

    add footer

---
+++ src/component/Footer/Footer.jsx
@@ -0,0 +1,59 @@
+import { Box, Paper } from '@material-ui/core';
+import React from 'react';
+import './style.css'
+ 
+Footer.propTypes = {
+  
+};
+
+function Footer(props) {
+  return (
+    <Box>
+      <Paper elevation={0} variant="outlined" square>
+        <div className='footer'>
+          <div className="footer__links">
+            <ul className="link-columns">
+              <li>
+                <a href="#">About US</a>
+              </li>
+              <li>
+                <a href="#">Contact US</a>
+              </li>
+            </ul>
+            <ul className="link-columns">
+              <li>
+                <a href="#">Careers</a>
+              </li>
+              <li>
+                <a href="#">Blog</a>
+              </li>
+              <li>
+                <a href="#">Help and Support</a>
+              </li>
+            </ul>
+            <ul className="link-columns">
+              <li>
+                <a href="#">Term</a>
+              </li>
+              <li>
+                <a href="#">Privacy policy</a>
+              </li>
+              <li>
+                <a href="#">Sitemap</a>
+              </li>
+            </ul>
+          </div>
+          <hr />
+          <div className="footer__logo-and-copyright">
+            <ul className="logo-and-copyright">
+              <li>Bryan Shop</li>
+              <li>© 2021 BryanTran, Inc.</li>
+            </ul>
+          </div>  
+        </div>
+      </Paper>
+    </Box>
+  );
+}
+
+export default Footer;
