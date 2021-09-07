ed1a405be04e5d4d961340f61bdf8d269e2b6b03
namthanhtran <namthanhtran16@gmail.com>
2021-09-07 13:12:39 +0700

    change route DetailPage

--- src/features/Products/index.jsx
+++ src/features/Products/index.jsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Route, Switch, useRouteMatch } from 'react-router-dom';
-import ListPage from './pages/ListPage';
-import DetailPage from './pages/DetailPage';
 import { Box } from '@material-ui/core';
+import React from 'react';
+import { Route, Switch, useRouteMatch } from 'react-router-dom';
+import DetailPage from './pages/DetailPage';
+import ListPage from './pages/ListPage';
 
 ProductFeature.propTypes = {
   
@@ -11,11 +10,12 @@ ProductFeature.propTypes = {
 
 function ProductFeature(props) {
   const match = useRouteMatch();
+  
   return (
     <Box pt={4}>
       <Switch>
         <Route path={match.path} exact component={ListPage}/>
-        <Route path={`${match.path}/:productId`} exact component={DetailPage}/>
+        <Route path={`${match.path}/:productId`} component={DetailPage}/>
       </Switch>
     </Box>
   );
