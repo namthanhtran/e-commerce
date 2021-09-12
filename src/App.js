faebbaea9c9760a08546893ac03e0dfd8e3bc97a
namthanhtran <namthanhtran16@gmail.com>
2021-09-12 21:20:22 +0700

    add route Cart

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Footer from 'component/Footer/Footer';
 import HomePage from 'component/HomePage/HomePage';
+import CartFeature from 'features/Cart';
 import ProductFeature from 'features/Products';
 import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
@@ -16,6 +17,7 @@ function App() {
         <Redirect from="/home" to="/" exact/>
         <Route path="/" component={HomePage} exact/>
         <Route path="/products" component={ProductFeature}/>
+        <Route path="/cart" component={CartFeature}/>
         <Route component={NotFound} />
       </Switch>
 
