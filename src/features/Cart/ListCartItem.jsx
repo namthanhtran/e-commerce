ed93f5aef01dcbbd7ea34edaa7c7d31189030351
namthanhtran <namthanhtran16@gmail.com>
2021-12-02 12:05:47 +0700

    handle change quantity in cart feature

--- src/features/Cart/ListCartItem.jsx
+++ src/features/Cart/ListCartItem.jsx
@@ -1,5 +1,5 @@
 import { yupResolver } from '@hookform/resolvers/yup';
-import { Box, Button, Grid, makeStyles, Typography } from '@material-ui/core';
+import { Box, Button, Grid, makeStyles, Paper, Table, TableBody, TableCell, TableHead, TableRow, Typography } from '@material-ui/core';
 import { STATIC_HOST, THUMBNAIL_PLACEHOLDER } from 'constants/index';
 import React from 'react';
 import { useForm } from 'react-hook-form';
@@ -7,7 +7,11 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router';
 import { formatPrice } from 'utils';
 import * as yup from 'yup';
-import { removeCartItem } from './cartSlice';
+import { addToCart, setQuantity, increaseQuantity, decreaseQuantity, changeQuantity, removeCartItem } from './cartSlice';
+import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline';
+import InputField from 'component/FormControls/InputField/InputField';
+import useProductDetail from '../Products/hooks/useProductDetail';
+import QuantityField from 'component/FormControls/QuantityField/QuantityField';
 
 const useStyles = makeStyles( theme => ({
   root: '',
@@ -15,54 +19,71 @@ const useStyles = makeStyles( theme => ({
     width: '100%',
     display: 'flex',
   },
-  img: {
-    width: '100px',
-    height: '100px',
-    margin: theme.spacing(0, 2),
-    padding: theme.spacing(1 , 1),
-    border: '1px solid #000'
-  },
-  boxText: {
-    width: '150px',
-    height: '100%',
+  product: {
     display: 'flex',
-    flexDirection: 'column',
-    alignItem: 'center',
-    justifyContent: 'left',
-    margin: theme.spacing(0, 1.5),
-  },
-  name: {
-    fontSize: '18px',
-    color: theme.palette.grey[800],
-    margin: theme.spacing(0, 1)
-  },
-  sku: {
-    fontSize: '14px',
-    color: theme.palette.grey[500],
-    margin: theme.spacing(0, 1)
+    alignItems: 'center',
   },
   quantity: {
-    width: '100px',
-    margin: theme.spacing(0, 2)
+    width: '50px',
+    height: '20px',
+    padding: '3px 5px',
+    border: '1px solid #000',
+    borderRadius: '5px'
   },
-  price: {
-    width: '150px',
-    margin: theme.spacing(0, 2),
-    fontSize: '18px',
-    fontWeight: 'semi-bold',
-    color: theme.palette.primary.main,
-  },
-  button: {
-    margin: theme.spacing(0, 2),
-    height: '30px',
-    color: theme.palette.primary.main,
-    border: `1px solid ${theme.palette.primary.dark}`
-  },
-  boxBack: {
+  paper: {
     width: '100%',
-    height: '100%',
+  },
+  back: {
+    width: '100%',
+    height: '100vh',
+    textAlign: 'center',
+  },
+  text: {
+    margin: '20px 0',
+  },
+  thumbnail: {
+    width: '100px',
+    height: '100px',
+  },
+  proName: {
+    marginLeft: '20px',
+  },
+  buton: {
+    color: '#000',
+    background: '#fff',
+    border: '2px solid #3f51b5',
+    '&:hover': {
+      color: '#fff',
+      background: '#3f51b5'
+    }
+  },
+  btnDelete: {
+    color: '#3f51b5',
+    '&:hover':{
+      cursor: 'pointer'
+    }
+  },
+  changeQuantity: {
+    width: '100px',
+    border: '1px solid #000',
     display: 'flex',
-    justifyContent: 'center'
+    justifyContents: 'space-between',
+    alignItems: 'center',
+  },
+  inputChange: {
+    textAlign: 'center',
+    border: 'none',
+    padding: '5px',
+    width: '50px',
+    height: '20px',
+  },
+  btnChange: {
+    display: 'inline-block',
+    background: '#fff',
+    border: 'none',
+    '&:hover':{
+      cursor: 'pointer',
+    }
   }
 }))
 
@@ -76,27 +97,27 @@ function ListCartItem(props) {
   const dispatch = useDispatch();
   const listItem = useSelector(state => state.cart.cartItems);
 
-  const schema = yup.object().shape({
-    
-    quantity: yup
-            .number()
-            .min(1, 'Please enter at least 1') 
-            .required('Please enter quantity')
-            .typeError('Please enter a number'),    
-  });
-
-  const form = useForm({
-    defaultValues: {
-      quantity: '1'
-    },
-    resolver: yupResolver(schema),
-  });
 
   const handleRemove = (id) => {
     const action = removeCartItem(id);
-    console.log(action);
     dispatch(action);
-  } 
+  }
+
+  const handleIncrease = (id, quantity) => {
+    const action = increaseQuantity({id, quantity});
+    dispatch(action);
+  }
+
+  const handleDecrease = (id, quantity) => {
+    console.log(id, quantity);
+    if(quantity <= 1){
+      const action = removeCartItem(id);
+      dispatch(action);
+      return;
+    }
+    const action = decreaseQuantity({id, quantity});
+    dispatch(action);
+  }
 
   const handleBuy = () => {
     history.push("/products")
@@ -106,31 +127,55 @@ function ListCartItem(props) {
     <Box>
       <Grid container>
         {listItem.length === 0 &&
-          <Box className={classes.boxBack}>
-            <Button onClick={handleBuy} className={classes.button} >
+          <Box className={classes.back}>
+            <Typography className={classes.text}>Không có sản phẩm nào trong giỏ hàng</Typography>
+            <Button onClick={handleBuy} className={classes.buton} >
               Mua sắm ngay
             </Button>
           </Box>
         }
-        {listItem.map(item => (
-          <li key={item.id} className={classes.flexItem}>
-            <Box>
-              <img src={item.product.thumbnail ? `${STATIC_HOST}${item.product.thumbnail.url}` : THUMBNAIL_PLACEHOLDER } className={classes.img} alt="" />
-            </Box>
-            <Box className={classes.boxText}>
-              <Typography className={classes.name}>{item.product.name}</Typography>
-              <Typography className={classes.sku}>SKU : {item.id}</Typography>
-            </Box>
-            <Box className={classes.quantity}>
-              Số lượng: {item.quantity}
-            </Box>
-            <Box className={classes.price}>
-              {formatPrice(item.product.salePrice * item.quantity)}
-            </Box>
-            <Button onClick={() => handleRemove(item.id)} className={classes.button}>Xóa</Button>
-          </li>
-        ))}
-      </Grid>
+        {listItem.length !== 0 &&
+          <Paper className={classes.paper}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Sản phẩm</TableCell>
+                  <TableCell>Đơn giá</TableCell>
+                  <TableCell>Số lượng</TableCell>
+                  <TableCell>Thành tiền</TableCell>
+                  <TableCell>Xóa</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {listItem.map(item => (
+                  <TableRow key={item.id}>
+                    <TableCell className={classes.product}>
+                      <img src={item.product.thumbnail ? `${STATIC_HOST}${item.product.thumbnail.url}` : THUMBNAIL_PLACEHOLDER} className={classes.thumbnail} alt="" />
+                      <Typography className={classes.proName}>{item.product.name}</Typography>
+                    </TableCell>
+                    <TableCell>
+                      <Typography>{formatPrice(item.product.salePrice)}</Typography>
+                    </TableCell>
+                    <TableCell>
+                      <Box className={classes.changeQuantity}>
+                        <button onClick={() => handleDecrease(item.id, item.quantity)} className={classes.btnChange}> - </button>
+                        <input type="text" value={item.quantity} className={classes.inputChange}/>
+                        <button onClick={() => handleIncrease(item.id, item.quantity)} className={classes.btnChange}> + </button>
+                      </Box>
+                    </TableCell>
+                    <TableCell>
+                      <Typography>{formatPrice(item.product.salePrice * item.quantity)}</Typography>
+                    </TableCell>
+                    <TableCell>
+                      <DeleteOutlineIcon onClick={() => handleRemove(item.id)} className={classes.btnDelete}/>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </Paper>
+        }
+      </Grid> 
     </Box>
   );
 }
